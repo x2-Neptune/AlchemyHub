1f70ed5fb67d5e6bd45e8ab85b59fd0a539563ff
x2Neptune <147920162+x2-Neptune@users.noreply.github.com>
2024-05-26 15:24:46 +0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -46,8 +46,117 @@
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢿⣦⣤⣤⡾⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ]]
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ZoiIntra/__Script/main/__Ads/__Name.HunterXHub"))()
-wait(.8)
+__f = {
+    ['__game'] = function()
+        local p = game.PlaceId
+        if p == 2753915549 then return "BloxFruits"
+        elseif p == 4442272183 then return "BloxFruits"
+        elseif p == 7449423635 then return "BloxFruits"
+        elseif p == 537413528 then return "BuildABoat"
+        else return "BladeBall" end
+    end;
+    ['__script'] = function(m) return "https://raw.githubusercontent.com/ZoiIntra/__Script/main/__Alchemy/__"..m..".lua" end;
+    ['__load'] = function(s) loadstring(game:HttpGet(s))() end;
+    ['__ismobile'] = function()
+        local uis = game:GetService("UserInputService")
+        if uis.TouchEnabled and not uis.KeyboardEnabled and not uis.MouseEnabled then return true
+        elseif not uis.TouchEnabled and uis.KeyboardEnabled and uis.MouseEnabled then return false end
+    end;
+}
+local __AdBanner = {}
+__AdBanner["1"] = Instance.new("ScreenGui", game:WaitForChild("CoreGui"));
+__AdBanner["1"]["Name"] = tostring(math.random(10000,999999));  
+__AdBanner["1"]["ZIndexBehavior"] = Enum.ZIndexBehavior.Sibling;
+__AdBanner["2"] = Instance.new("ImageButton", __AdBanner["1"]);
+__AdBanner["2"]["BorderSizePixel"] = 0;
+__AdBanner["2"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+__AdBanner["2"]["AnchorPoint"] = Vector2.new(0.5, 0.5);
+__AdBanner["2"]["Image"] = [[http://www.roblox.com/asset/?id=17414524839]];
+__AdBanner["2"]["Size"] = UDim2.new(0, 500, 0, 162);
+__AdBanner["2"]["Name"] = [[Mobile]];
+__AdBanner["2"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+__AdBanner["2"]["Position"] = UDim2.new(0.49974286556243896, 0, 0.4971980154514313, 0);
+__AdBanner["2"]["Visible"] = false
+__AdBanner["3"] = Instance.new("UICorner", __AdBanner["2"]);
+__AdBanner["4"] = Instance.new("UIStroke", __AdBanner["2"]);
+__AdBanner["4"]["Color"] = Color3.fromRGB(0, 255, 226);
+__AdBanner["4"]["Thickness"] = 0;
+__AdBanner["5"] = Instance.new("TextLabel", __AdBanner["2"]);
+__AdBanner["5"]["BorderSizePixel"] = 0;
+__AdBanner["5"]["TextYAlignment"] = Enum.TextYAlignment.Top;
+__AdBanner["5"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+__AdBanner["5"]["TextXAlignment"] = Enum.TextXAlignment.Right;
+__AdBanner["5"]["FontFace"] = Font.new([[rbxasset://fonts/families/GothamSSm.json]], Enum.FontWeight.Regular, Enum.FontStyle.Normal);
+__AdBanner["5"]["TextSize"] = 12;
+__AdBanner["5"]["TextColor3"] = Color3.fromRGB(255, 255, 255);
+__AdBanner["5"]["Size"] = UDim2.new(0, 499, 0, 11);
+__AdBanner["5"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+__AdBanner["5"]["Text"] = [[หากสนใจลงโฆษณาติดต่อผ่าน ticket ใน discord.gg/alchemyhub]];
+__AdBanner["5"]["Name"] = [[TH]];
+__AdBanner["5"]["BackgroundTransparency"] = 1;
+__AdBanner["5"]["Position"] = UDim2.new(0, 0, 1.100000023841858, 0);
+__AdBanner["6"] = Instance.new("TextLabel", __AdBanner["2"]);
+__AdBanner["6"]["BorderSizePixel"] = 0;
+__AdBanner["6"]["TextYAlignment"] = Enum.TextYAlignment.Top;
+__AdBanner["6"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+__AdBanner["6"]["TextXAlignment"] = Enum.TextXAlignment.Right;
+__AdBanner["6"]["FontFace"] = Font.new([[rbxasset://fonts/families/GothamSSm.json]], Enum.FontWeight.Regular, Enum.FontStyle.Normal);
+__AdBanner["6"]["TextSize"] = 12;
+__AdBanner["6"]["TextColor3"] = Color3.fromRGB(255, 255, 255);
+__AdBanner["6"]["Size"] = UDim2.new(0, 499, 0, 11);
+__AdBanner["6"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+__AdBanner["6"]["Text"] = [[If interested in advertising, contact via ticket at discord.gg/alchemyhub]];
+__AdBanner["6"]["Name"] = [[ENG]];
+__AdBanner["6"]["BackgroundTransparency"] = 1;
+__AdBanner["6"]["Position"] = UDim2.new(0, 0, 1.023077130317688, 0);
+__AdBanner["7"] = Instance.new("ImageButton", __AdBanner["1"]);
+__AdBanner["7"]["BorderSizePixel"] = 0;
+__AdBanner["7"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+__AdBanner["7"]["AnchorPoint"] = Vector2.new(0.5, 0.5);
+__AdBanner["7"]["Image"] = [[http://www.roblox.com/asset/?id=17414524839]];
+__AdBanner["7"]["Size"] = UDim2.new(0, 820, 0, 260);
+__AdBanner["7"]["Name"] = [[PC]];
+__AdBanner["7"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+__AdBanner["7"]["Visible"] = false;
+__AdBanner["7"]["Position"] = UDim2.new(0.49974289536476135, 0, 0.4997042715549469, 0);
+__AdBanner["7"]["Visible"] = false
+__AdBanner["8"] = Instance.new("UICorner", __AdBanner["7"]);
+__AdBanner["9"] = Instance.new("UIStroke", __AdBanner["7"]);
+__AdBanner["9"]["Color"] = Color3.fromRGB(0, 255, 226);
+__AdBanner["9"]["Thickness"] = 0;
+__AdBanner["a"] = Instance.new("TextLabel", __AdBanner["7"]);
+__AdBanner["a"]["BorderSizePixel"] = 0;
+__AdBanner["a"]["TextYAlignment"] = Enum.TextYAlignment.Top;
+__AdBanner["a"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+__AdBanner["a"]["TextXAlignment"] = Enum.TextXAlignment.Right;
+__AdBanner["a"]["FontFace"] = Font.new([[rbxasset://fonts/families/GothamSSm.json]], Enum.FontWeight.Regular, Enum.FontStyle.Normal);
+__AdBanner["a"]["TextSize"] = 16;
+__AdBanner["a"]["TextColor3"] = Color3.fromRGB(255, 255, 255);
+__AdBanner["a"]["Size"] = UDim2.new(0, 820, 0, 20);
+__AdBanner["a"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+__AdBanner["a"]["Text"] = [[If interested in advertising, contact via ticket at discord.gg/alchemyhub]];
+__AdBanner["a"]["Name"] = [[ENG]];
+__AdBanner["a"]["BackgroundTransparency"] = 1;
+__AdBanner["a"]["Position"] = UDim2.new(7.443311744737002e-08, 0, 1.0230770111083984, 0);
+__AdBanner["b"] = Instance.new("TextLabel", __AdBanner["7"]);
+__AdBanner["b"]["BorderSizePixel"] = 0;
+__AdBanner["b"]["TextYAlignment"] = Enum.TextYAlignment.Top;
+__AdBanner["b"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+__AdBanner["b"]["TextXAlignment"] = Enum.TextXAlignment.Right;
+__AdBanner["b"]["FontFace"] = Font.new([[rbxasset://fonts/families/GothamSSm.json]], Enum.FontWeight.Regular, Enum.FontStyle.Normal);
+__AdBanner["b"]["TextSize"] = 16;
+__AdBanner["b"]["TextColor3"] = Color3.fromRGB(255, 255, 255);
+__AdBanner["b"]["Size"] = UDim2.new(0, 820, 0, 20);
+__AdBanner["b"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+__AdBanner["b"]["Text"] = [[หากสนใจลงโฆษณาติดต่อผ่าน ticket ใน discord.gg/alchemyhub]];
+__AdBanner["b"]["Name"] = [[TH]];
+__AdBanner["b"]["BackgroundTransparency"] = 1;
+__AdBanner["b"]["Position"] = UDim2.new(7.443311744737002e-08, 0, 1.0769232511520386, 0);
+if __f['__ismobile']() then __AdBanner["2"]["Visible"] = true
+else __AdBanner["7"]["Visible"] = true end
+__AdBanner["7"].MouseButton1Click:Connect(function() if setclipboard then setclipboard('https://discord.gg/wXezdGtWsW') end end)
+__AdBanner["2"].MouseButton1Click:Connect(function() if setclipboard then setclipboard('https://discord.gg/wXezdGtWsW') end end)
+wait(5); __AdBanner["1"]:Destroy()
 if _G.StreamerMode then
     pcall(function()
         local a_protect = game:GetDescendants()
@@ -64,37 +173,5 @@ if _G.StreamerMode then
         end
     end)
 end
-if not _G.Aimbot then
-    __Luable = {["ACM"] = {"discord.gg/alchemyhub",{{game.PlaceId,{"__BloxFruits","__BuildABoat","__KingLegacy","__BladeBall","__SolsRNG"}},
-    {tostring, loadstring},{"luable.top","<3"},{"x2neptune.tech"}},{3, 5, 1, 4, 2}}}i = {(__Luable["ACM"])};v = {"iRS", i[1]};
-    __ACM = {["A"] = v[2],["C"] = i,["M"] = {".".."\108\117\97",true,"https://raw.githubusercontent.com/ZoiIntra/__Script/main/__Alchemy/"}}
-    __AlchemyOnTop = {function(__P)
-    if __P == 2753915549 then return {__ACM["A"][__ACM["C"][1][3][5]][__ACM["C"][1][3][5]][__ACM["C"][1][3][3]](__ACM["A"][__ACM["C"][1][3][5]][__ACM["C"][1][3][3]][__ACM["C"][1][3][5]][__ACM["C"][1][3][3]])}
-    elseif __P == 4442272183 then return {__ACM["A"][__ACM["C"][1][3][5]][__ACM["C"][1][3][5]][__ACM["C"][1][3][3]](__ACM["A"][__ACM["C"][1][3][5]][__ACM["C"][1][3][3]][__ACM["C"][1][3][5]][__ACM["C"][1][3][3]])}
-    elseif __P == 7449423635 then return {__ACM["A"][__ACM["C"][1][3][5]][__ACM["C"][1][3][5]][__ACM["C"][1][3][3]](__ACM["A"][__ACM["C"][1][3][5]][__ACM["C"][1][3][3]][__ACM["C"][1][3][5]][__ACM["C"][1][3][3]])}
-    elseif __P == 537413528 then return {__ACM["A"][__ACM["C"][1][3][5]][__ACM["C"][1][3][5]][__ACM["C"][1][3][3]](__ACM["A"][__ACM["C"][1][3][5]][__ACM["C"][1][3][3]][__ACM["C"][1][3][5]][__ACM["C"][1][3][5]])}
-    elseif __P == 4520749081 then return {__ACM["A"][__ACM["C"][1][3][5]][__ACM["C"][1][3][5]][__ACM["C"][1][3][3]](__ACM["A"][__ACM["C"][1][3][5]][__ACM["C"][1][3][3]][__ACM["C"][1][3][5]][3])}
-    elseif __P == 6381829480 then return {__ACM["A"][__ACM["C"][1][3][5]][__ACM["C"][1][3][5]][__ACM["C"][1][3][3]](__ACM["A"][__ACM["C"][1][3][5]][__ACM["C"][1][3][3]][__ACM["C"][1][3][5]][3])}
-    elseif __P == 15532962292 then return {__ACM["A"][__ACM["C"][1][3][5]][__ACM["C"][1][3][5]][__ACM["C"][1][3][3]](__ACM["A"][__ACM["C"][1][3][5]][__ACM["C"][1][3][3]][__ACM["C"][1][3][5]][5])}
-    else return {__ACM["A"][__ACM["C"][1][3][5]][__ACM["C"][1][3][5]][__ACM["C"][1][3][3]](__ACM["A"][__ACM["C"][1][3][5]][__ACM["C"][1][3][3]][__ACM["C"][1][3][5]][4])} end
-    end}
-    __ACM["A"][__ACM["C"][1][3][5]][__ACM["C"][1][3][5]][__ACM["C"][1][3][5]](game:HttpGet(
-    __ACM["M"][3]..__ACM["A"][__ACM["C"][1][3][5]][__ACM["C"][1][3][5]][__ACM["C"][1][3][3]](
-    __AlchemyOnTop[__ACM["C"][1][3][3]](__ACM["A"][__ACM["C"][1][3][5]]
-    [__ACM["C"][1][3][3]][__ACM["C"][1][3][3]])[__ACM["C"][1][3][3]])..__ACM["M"][1]
-    ))()
-else
-    __Luable = {["ACM"] = {"discord.gg/alchemyhub",{{game.PlaceId,{"__Aimbot","__Aimgay","__Aimslient","__Aimkui","GGEzRunner"}},
-    {tostring, loadstring},{"luable.top","<3"},{"x2neptune.tech"}},{3, 5, 1, 4, 2}}}i = {(__Luable["ACM"])};v = {"iRS", i[1]};
-    __ACM = {["A"] = v[2],["C"] = i,["M"] = {".".."\108\117\97",true,"https://raw.githubusercontent.com/ZoiIntra/__Script/main/__Alchemy/"}}
-    __AlchemyOnTop = {function(__P)
-    if __P == 2753915549 then return {__ACM["A"][__ACM["C"][1][3][5]][__ACM["C"][1][3][5]][__ACM["C"][1][3][3]](__ACM["A"][__ACM["C"][1][3][5]][__ACM["C"][1][3][3]][__ACM["C"][1][3][5]][__ACM["C"][1][3][3]])}
-    elseif __P == 4442272183 then return {__ACM["A"][__ACM["C"][1][3][5]][__ACM["C"][1][3][5]][__ACM["C"][1][3][3]](__ACM["A"][__ACM["C"][1][3][5]][__ACM["C"][1][3][3]][__ACM["C"][1][3][5]][__ACM["C"][1][3][3]])}
-    elseif __P == 7449423635 then return {__ACM["A"][__ACM["C"][1][3][5]][__ACM["C"][1][3][5]][__ACM["C"][1][3][3]](__ACM["A"][__ACM["C"][1][3][5]][__ACM["C"][1][3][3]][__ACM["C"][1][3][5]][__ACM["C"][1][3][3]])} end
-    end}
-    __ACM["A"][__ACM["C"][1][3][5]][__ACM["C"][1][3][5]][__ACM["C"][1][3][5]](game:HttpGet(
-    __ACM["M"][3]..__ACM["A"][__ACM["C"][1][3][5]][__ACM["C"][1][3][5]][__ACM["C"][1][3][3]](
-    __AlchemyOnTop[__ACM["C"][1][3][3]](__ACM["A"][__ACM["C"][1][3][5]]
-    [__ACM["C"][1][3][3]][__ACM["C"][1][3][3]])[__ACM["C"][1][3][3]])..__ACM["M"][1]
-    ))()
-end
+if _G.Aimbot then __f['__load'](__f['__script']("Aimbot"))
+else __f['__load'](__f['__script'](__f['__game']())) end

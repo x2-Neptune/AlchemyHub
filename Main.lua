943e4618a8fb98883b4ea215fdade3178c42d826
x2Neptune <147920162+x2-Neptune@users.noreply.github.com>
2024-12-21 22:00:27 +0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -215,9 +215,7 @@ _G.run_time = true
 pcall(function() loadstring(game:HttpGet("https://raw.githubusercontent.com/ZoiIntra/__Script/refs/heads/main/__Finded.lua"))() end)
 if isExecutors("delta") then loadWarn("Hello!, we detected that your executor is Delta, we recommend using ArceusX or Supported Executor, because Delta not Support Our Fully Scripts.","Delta") end;
 if isExecutors("solara") then loadWarn("Hello!, we detected that your executor is Solara, we recommend using Xeno ( On fisch ), Swift or Paid Executor, because Solara not Support Our Fully Scripts.","Solara") end;
-if _G.Aimbot then __f['__load'](__f['__script']("Aimbot"))
-elseif _G.FruitFinder then __f['__load'](__f['__script']("FruitFinder"))
-elseif _G.AutoBounty then __f['__load'](__f['__script']("AutoBounty"))
-elseif _G.Old then __f['__load'](__f['__script']("BloxFruitsOld"))
-elseif _G.VisualFarm then loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/23a949918d0b9965966a3ef777171127.lua"))();
-else getgenv().loader = __f['__game'](); loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/6df239f7eb1c8bb019d8c87e92784540.lua"))() end;
+if _G.Aimbot then __f['__load']("https://api.luarmor.net/files/v3/loaders/99d54b8a79622e362bca83739647c514.lua")
+elseif _G.FruitFinder then game.Players.LocalPlayer:Kick("Coming Soon!")
+elseif _G.AutoBounty then game.Players.LocalPlayer:Kick("Coming Soon!")
+else getgenv().loader = __f['__game'](); __f['__load']("https://api.luarmor.net/files/v3/loaders/6df239f7eb1c8bb019d8c87e92784540.lua") end;

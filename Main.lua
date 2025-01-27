ce54ee364021f8f03b9c8a288f026120ba8e2a86
x2Neptune <147920162+x2-Neptune@users.noreply.github.com>
2025-01-27 12:30:34 +0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -128,87 +128,8 @@ Notification.new({
 		}
 	}
 })
-local loadWarn = function(__text,__exec)
-    local warnING = Instance.new("ScreenGui")
-    local MainFrame = Instance.new("Frame")
-    local MainCorner = Instance.new("UICorner")
-    local ImageLabel = Instance.new("ImageLabel")
-    local Submit = Instance.new("TextButton")
-    local SubmitCorner = Instance.new("UICorner")
-    local Information = Instance.new("TextLabel")
-    local Header = Instance.new("TextLabel")
-    warnING.Name = "warnING"
-    warnING.Parent = game:WaitForChild("CoreGui")
-    warnING.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-    MainFrame.Name = "MainFrame"
-    MainFrame.Parent = warnING
-    MainFrame.AnchorPoint = Vector2.new(0.5, 0.5)
-    MainFrame.BackgroundColor3 = Color3.fromRGB(20, 20, 20)
-    MainFrame.Position = UDim2.new(0.499723464, 0, 0.499925047, 0)
-    MainFrame.Size = UDim2.new(0, 0, 0, 0)
-    MainFrame.ZIndex = 0
-    MainCorner.Name = "MainCorner"
-    MainCorner.Parent = MainFrame
-    ImageLabel.Parent = MainFrame
-    ImageLabel.AnchorPoint = Vector2.new(0.5, 0.5)
-    ImageLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-    ImageLabel.BackgroundTransparency = 1.000
-    ImageLabel.BorderColor3 = Color3.fromRGB(0, 0, 0)
-    ImageLabel.BorderSizePixel = 0
-    ImageLabel.Position = UDim2.new(0.498768479, 0, 0.493386239, 0)
-    ImageLabel.Size = UDim2.new(0, 188, 0, 188)
-    ImageLabel.Image = "rbxassetid://696390628"
-    ImageLabel.ImageTransparency = 0.900
-    Submit.Name = "Submit"
-    Submit.Parent = MainFrame
-    Submit.AnchorPoint = Vector2.new(0.5, 0.5)
-    Submit.BackgroundColor3 = Color3.fromRGB(152, 152, 152)
-    Submit.BackgroundTransparency = 0.800
-    Submit.Position = UDim2.new(0.498482376, 0, 0.773373246, 0)
-    Submit.Size = UDim2.new(0, 355, 0, 30)
-    Submit.Font = Enum.Font.GothamBold
-    Submit.Text = "I understand but I want to continue using "..__exec.."."
-    Submit.TextColor3 = Color3.fromRGB(255, 255, 255)
-    Submit.TextSize = 14.000
-    SubmitCorner.CornerRadius = UDim.new(0, 5)
-    SubmitCorner.Name = "SubmitCorner"
-    SubmitCorner.Parent = Submit
-    Information.Name = "Information"
-    Information.Parent = MainFrame
-    Information.BackgroundColor3 = Color3.fromRGB(255, 254, 216)
-    Information.BackgroundTransparency = 1.000
-    Information.ClipsDescendants = true
-    Information.Position = UDim2.new(0.0834580585, 0, 0.340372086, 0)
-    Information.Size = UDim2.new(0, 336, 0, 60)
-    Information.Font = Enum.Font.GothamBold
-    Information.Text = __text
-    Information.TextColor3 = Color3.fromRGB(255, 254, 216)
-    Information.TextSize = 15.000
-    Information.TextWrapped = true
-    Information.TextYAlignment = Enum.TextYAlignment.Top
-    Header.Name = "Header"
-    Header.Parent = MainFrame
-    Header.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-    Header.BackgroundTransparency = 1.000
-    Header.ClipsDescendants = true
-    Header.Position = UDim2.new(0.0295566507, 0, 0.128732026, 0)
-    Header.Size = UDim2.new(0, 381, 0, 39)
-    Header.Font = Enum.Font.GothamBold
-    Header.Text = "Executor Warning"
-    Header.TextColor3 = Color3.fromRGB(255, 255, 255)
-    Header.TextSize = 32.000
-    Submit.MouseButton1Click:Connect(function()
-        warnING.Enabled = false
-    end)
-    wait(); MainFrame:TweenSize(UDim2.new(0, 406, 0, 189), Enum.EasingDirection.Out, Enum.EasingStyle.Quart, 0.3, true)
-end
 _G.run_time = true
 pcall(function() loadstring(game:HttpGet("https://raw.githubusercontent.com/ZoiIntra/__Script/refs/heads/main/__Finded.lua"))() end)
-pcall(function()
-    Service.TextChannels.RBXSystem:DisplaySystemMessage("[ Alchemy Script ] <font color='#ffff00'>Connect to Luarmor</font>")
-end)
---if isExecutors("delta") then loadWarn("Hello!, we detected that your executor is Delta, we recommend using ArceusX or Supported Executor, because Delta not Support Our Fully Scripts.","Delta") end;
---if isExecutors("solara") then loadWarn("Hello!, we detected that your executor is Solara, we recommend using Xeno ( On fisch ), Swift or Paid Executor, because Solara not Support Our Fully Scripts.","Solara") end;
 if _G.Aimbot then __f['__load']("https://api.luarmor.net/files/v3/loaders/99d54b8a79622e362bca83739647c514.lua")
 elseif _G.FruitFinder then loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/53bdb753f44b36ddb22efdd03c1bdd2f.lua"))()
 elseif _G.AutoBounty then game.Players.LocalPlayer:Kick("Coming Soon!")
